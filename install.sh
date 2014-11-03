968e16c34f2339d60b89f4f0d86c06c13de2c6c6
ssoi <(none)>
2014-11-03 13:08:05 -0500

    Add tmux config file

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -fs ${DFILES}/vimrc ${HOME}/.vimrc
 ln -fs ${DFILES}/funcs.R ${HOME}/funcs.R 
 ln -fs ${DFILES}/Rprofile ${HOME}/.Rprofile
 ln -fs ${DFILES}/gitconfig ${HOME}/.gitconfig
+ln -fs ${DFILES}/tmuxconf ${HOME}/.tmux.conf
