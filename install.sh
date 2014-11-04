80c54c36bdfb50643f44ab91f4c9509d22673554
ssoi <(none)>
2014-11-04 11:21:00 -0500

    Add conky config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -fs ${DFILES}/funcs.R ${HOME}/funcs.R
 ln -fs ${DFILES}/Rprofile ${HOME}/.Rprofile
 ln -fs ${DFILES}/gitconfig ${HOME}/.gitconfig
 ln -fs ${DFILES}/tmuxconf ${HOME}/.tmux.conf
+ln -fs ${DFILES}/conkyrc ${HOME}/.conkyrc
