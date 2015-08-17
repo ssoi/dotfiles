aa82edb8ddd73cdda00ee0bd651e6dff7ab40f4a
ssoi <sameer@betterpath.com>
2015-08-17 14:41:53 -0700

    feat(osx): add tasks for mac

--- firstboot.sh
+++ firstboot.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
+BASHRC=".bash_profile"
 # install some necessary packages
-deb http://lib.stat.cmu.edu/R/CRAN//bin/linux/ubuntu trusty/ # add R repo
-apt-add-repository -y "deb http://repository.spotify.com stable non-free" # add spotify repo
-add-apt-repository -y ppa:chromium-daily/stable # add chromium repo
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 # add R key
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 # add spotify key
-apt-get update
-apt-get update
-apt-get install vim-gtk r-base-dev littler conky tmux chromium-browser \
-                arandr curl htop silversearcher-ag
+uname -a | grep -il darwin
+if [ $? -eq 0 ]
+then
+	brew update
+	brew upgrade
+	brew install tmux
+	brew install the_silver_searcher
+	brew install findutils --default-names
+	brew install gnu-sed --default-names
+	brew install gnu-tar --default-names
+	brew install gnu-which --default-names
+	brew install gnutls --default-names
+	brew install grep --default-names
+	brew install coreutils
+	brew install binutils
+	brew install diffutils
+	brew install gzip
+	brew install wget
+	brew install htop
+  brew tap homebrew/science
+  brew install gcc
+  brew install Caskroom/cask/xquartz
+	brew install r --with-openblas
+	brew install vim --override-system-vi
+	BASHRC=".bashrc"
+else
+	deb http://lib.stat.cmu.edu/R/CRAN//bin/linux/ubuntu trusty/ # add R repo
+	apt-add-repository -y "deb http://repository.spotify.com stable non-free" # add spotify repo
+	add-apt-repository -y ppa:chromium-daily/stable # add chromium repo
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 # add R key
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 # add spotify key
+	apt-get update
+	apt-get install vim-gtk r-base-dev littler conky tmux chromium-browser \
+	                arandr curl htop silversearcher-ag
+fi
 
 # install rvm and ruby versions
 \curl -sSL https://get.rvm.io | bash
@@ -31,4 +58,4 @@ git clone https://github.com/derekwyatt/vim-scala.git
 git clone https://github.com/vim-ruby/vim-ruby.git
 
 # customize bashrc
-echo "source `pwd`/bashcustom" >> $HOME/.bashrc
+echo "source `pwd`/bashcustom" >> $HOME/$BASHRC
--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 DFILES=`pwd`
+
+uname -a | grep -il darwin
+if [ ! $? -eq 0 ]
+then
+	ln -fs ${DFILES}/conkyrc ${HOME}/.conkyrc
+fi
+
 ln -fs ${DFILES}/vimrc ${HOME}/.vimrc 
 ln -fs ${DFILES}/funcs.R ${HOME}/funcs.R 
 ln -fs ${DFILES}/Rprofile ${HOME}/.Rprofile
 ln -fs ${DFILES}/gitconfig ${HOME}/.gitconfig
 ln -fs ${DFILES}/tmuxconf ${HOME}/.tmux.conf
-ln -fs ${DFILES}/conkyrc ${HOME}/.conkyrc
