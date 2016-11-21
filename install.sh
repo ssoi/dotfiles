0f206dec9dfcfeb9f0e51012539b62ff2108b785
ssoi <sameer@betterpath.com>
2016-11-21 17:03:57 -0500

    feat(i3config): add more sane bindings

--- install.sh
+++ install.sh
@@ -7,12 +7,8 @@ then
 	ln -fs ${DFILES}/conkyrc ${HOME}/.conkyrc
 fi
 
-if [[ -f ${HOME}/.i3/config ]]
-then
-  rm ${HOME}/.i3/config
-  ln -fs ${DFILES}/i3config ${HOME}/.i3/config
-fi
 
+ln -fs ${DFILES}/i3config ${HOME}/.i3/config
 ln -fs ${DFILES}/vimrc ${HOME}/.vimrc 
 ln -fs ${DFILES}/funcs.R ${HOME}/funcs.R 
 ln -fs ${DFILES}/Rprofile ${HOME}/.Rprofile
