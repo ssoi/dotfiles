d0d3774872624f89c0a9ad0b5d2d5f1158764edd
ssoi <sameer@betterpath.com>
2016-11-28 19:06:54 -0500

    feat(cycle-wallpaper): add script to cycle through wallpaper and schedule cron job

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ ln -fs ${DFILES}/funcs.R ${HOME}/funcs.R
 ln -fs ${DFILES}/Rprofile ${HOME}/.Rprofile
 ln -fs ${DFILES}/gitconfig ${HOME}/.gitconfig
 ln -fs ${DFILES}/tmuxconf ${HOME}/.tmux.conf
+ln -fs ${DFILES}/scripts/cycle-wallpaper /etc/cron.daily/cycle-wallpaper
