3e6f838dd665d06657ac3e7c9bd208c6b9a9d86f
ssoi <sameer@driver.xyz>
2017-04-14 13:27:04 -0400

    feat(i3): usability updates

--- firstboot.sh
+++ firstboot.sh
@@ -58,10 +58,13 @@ mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
 cd $HOME/.vim/bundle
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/kien/ctrlp.vim.git
+git clone git://github.com/tpope/vim-fugitive.git
 git clone https://github.com/vim-airline/vim-ariline.git
 git clone https://github.com/derekwyatt/vim-scala.git
 git clone https://github.com/vim-ruby/vim-ruby.git
 git clone https://github.com/Valloric/YouCompleteMe.git
+
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
 cd YouCompleteMe
 git submodule update --init --recursive
 ./install.py
